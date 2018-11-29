8c25a218975ed7fdf708af496f3012e05ddf3efc
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2018-11-29 20:43:54 -0200

    remove lazy-cache

--- utils.js
+++ utils.js
@@ -1,17 +1,23 @@
 'use strict';
 
-var utils = require('lazy-cache')(require);
-var fn = require;
-require = utils;
+// var utils = require('lazy-cache')(require);
+// var fn = require;
+// require = utils;
 
 /**
  * Lazily required module dependencies
  */
 
-require('debug');
-require('is-registered');
-require('is-valid-instance', 'isValid');
-require = fn;
+// require('debug');
+// require('is-registered');
+// require('is-valid-instance', 'isValid');
+// require = fn;
+
+var utils = {
+  debug: require('debug'),
+  'is-registered': require('is-registered'),
+  'is-valid-instance': require('is-valid-instance')
+};
 
 /**
  * Expose `utils` modules
